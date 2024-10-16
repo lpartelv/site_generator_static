52fbe6ccdf0b5c634a42551d6d28e174b8635b09
Laur Pärtel <laur.p2rtel@.com>
2024-10-16 11:11:23 +0300

    added parent node class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,20 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super(ParentNode, self).__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("MUST HAVE A TAG")
+        if self.children == None:
+            raise ValueError("MUST HAVE CHILDREN")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, Children: {self.children}, {self.props})"
